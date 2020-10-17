106fb4034908289221308a3630686684f991c6ad
davemanlimos <72651264+davemanlimos@users.noreply.github.com>
2020-10-17 20:06:32 +0800

    Add files via upload

---
+++ 00000021.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void main()
+{
+
+  int i, j, k ;
+  clrscr();
+
+  for(i = 1; i <= 24; i++)
+  {
+   for(j = 24-1; j >= i; j--)
+   {
+      printf(" ");
+   }
+   for(k = 1; k <= i; k++)
+   {
+      printf("*");
+   }
+   printf("\n");
+
+}
+
+   getch();
+}
+
